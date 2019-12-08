13c39fb0f61b5629b062218376bfc338f832e5e6
Tim Rader <tim.rader@otto.de>
2019-12-08 19:06:25 +0100

    Add test properties to enable tests to run successful with H2

--- src/test/java/de/cloud/fundamentals/distributor/DistributorApplicationTests.java
+++ src/test/java/de/cloud/fundamentals/distributor/DistributorApplicationTests.java
@@ -2,8 +2,10 @@ package de.cloud.fundamentals.distributor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DistributorApplicationTests {
 
 	@Test
