aa0452408155c49a11e4f3d2e0b0a8172cd6e194
Tim Rader <tim.rader@otto.de>
2019-12-08 16:26:24 +0100

    Remove schema definition from entity

--- src/main/java/de/cloud/fundamentals/distributor/persistence/domain/ClientEntity.java
+++ src/main/java/de/cloud/fundamentals/distributor/persistence/domain/ClientEntity.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "clients", schema = "telegram")
+@Table(name = "clients")
 public class ClientEntity {
 
     @Id
