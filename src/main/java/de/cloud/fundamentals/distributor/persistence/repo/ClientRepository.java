e22bcd4399bfd4432ca904f82ee6e42f8d09d9c7
Tim Rader <tim.rader@otto.de>
2019-12-08 16:41:35 +0100

    Remove table definition of client entity

--- src/main/java/de/cloud/fundamentals/distributor/persistence/repo/ClientRepository.java
+++ src/main/java/de/cloud/fundamentals/distributor/persistence/repo/ClientRepository.java
@@ -4,7 +4,6 @@ import de.cloud.fundamentals.distributor.persistence.domain.ClientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface ClientRepository extends JpaRepository<ClientEntity, Long> {
 
